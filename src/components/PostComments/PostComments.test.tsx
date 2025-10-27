b0633e9bd3cb9acecd22b02748009e16eee12e8c
devrafcks <rafcksps@gmail.com>
2025-10-27 00:38:38 -0300

    resolvendo exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,24 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+describe('PostComment', () => {
+    it('renderiza corretamente', () => {
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('adiciona dois comentários', () => {
+        render(<PostComment />);
+
+        const textarea = screen.getByTestId('comment-textarea');
+        const button = screen.getByTestId('comment-button');
+
+        fireEvent.change(textarea, { target: { value: 'teste comentario 1' } });
+        fireEvent.click(button);
+
+        fireEvent.change(textarea, { target: { value: 'teste comentario 2' } });
+        fireEvent.click(button);
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
